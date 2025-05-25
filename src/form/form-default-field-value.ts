d969f2f2fa7019db0eb42b9fbb2deb81c803fb3e
Marian Zburlea <m@wowjob.dev>
2025-05-25 15:27:55 +0400

    fix: default value field

--- src/form/form-default-field-value.ts
+++ src/form/form-default-field-value.ts
@@ -1,20 +1,29 @@
 import type { TJSONForm } from '@wowjob/ui'
 
-export function setFormDefaultFieldValue<T extends Record<string, unknown>>(
+export function setFormDefaultFieldValue<
+  T extends Partial<
+    Record<string, string | number | readonly string[] | undefined>
+  >
+>(
   formDataStructure: TJSONForm['formStructure'],
   defaultValueMap?: T
 ): TJSONForm['formStructure'] {
-  if (defaultValueMap) {
-    const fs = structuredClone(formDataStructure)
+  if (!defaultValueMap) return formDataStructure
 
-    for (const key of fs.list) {
-      if (key in fs.data) {
-        fs.data[key].defaultValue = defaultValueMap[key]
+  const fs = structuredClone(formDataStructure)
+
+  for (const key of fs.list) {
+    if (key in fs.data) {
+      const field = fs.data[key]
+      if (field?.defaultValue !== undefined) {
+        const val = defaultValueMap[key]
+        // TS knows val is string|number|string[]|undefined
+        if (val !== undefined) {
+          field.defaultValue = val
+        }
       }
     }
-
-    return fs
   }
 
-  return formDataStructure
+  return fs
 }
